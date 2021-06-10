9b1c3b49fba034bf3e519c52f21f39515b02c444
HolCa <85104469+HolCa@users.noreply.github.com>
2021-06-10 22:17:12 +0200

    AccordionItem neuer Link zu Google Docs Mockup

--- src/components/AccordionBtn/AccordionItem/AccordionItem.js
+++ src/components/AccordionBtn/AccordionItem/AccordionItem.js
@@ -3,7 +3,9 @@ import Card from 'react-bootstrap/Card';
 // import ListGroup from 'react-bootstrap/ListGroup';
 
 const AccordionItem = ({ item }) => {
-  return <Card.Link href="https://docs.google.com/presentation/d/15AwZvtffGKH-FROIwXgXR6l5G6pepxQrFCYqLDDgM4w/edit#slide=id.gdbe3aa83f0_0_0">{item}</Card.Link>;
+  return (
+      <Card.Link href="https://docs.google.com/presentation/d/15AwZvtffGKH-FROIwXgXR6l5G6pepxQrFCYqLDDgM4w/edit#slide=id.gdbe3aa83f0_0_0">{item}</Card.Link>
+  );
 };
 
 export default AccordionItem;
